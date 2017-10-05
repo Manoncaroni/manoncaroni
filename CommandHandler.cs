445d0eaa459898b00530e8f3f21c56dfa573bc27
Manoncaroni <32497249+Manoncaroni@users.noreply.github.com>
2017-10-04 18:31:43 -0700

    Create CommandHandler.cs

---
+++ CommandHandler.cs
@@ -0,0 +1,50 @@
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCari
+{
+  
+    public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+
+        private CommandService _service;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _service = new CommandService();
+
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+            if (msg.HasCharPrefix('!', ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+
+                if (!result.IsSuccess)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+        }
+        
+    }
+
+}
